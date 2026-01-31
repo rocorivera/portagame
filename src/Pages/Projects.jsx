0cfe587def0cc6db812e87957ac0c2bf5e8606cf
rober contreras <trebor.contrerasrivera@gmail.com>
2026-01-31 12:52:56 -0500

    reparacion

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -11,7 +11,7 @@ import PageWrapper from '../components/PageWrapper.jsx'
 
 const Projects = () => {
   const [tituloAnimado, setTituloAnimado] = useState(tituloCompleto)
-  const tituloCompleto = '🪄 Proyectos en Acción'
+  const tituloCompleto = "🪄 Proyectos en Acción"
 
   useEffect(() => {
     setTituloAnimado('');
